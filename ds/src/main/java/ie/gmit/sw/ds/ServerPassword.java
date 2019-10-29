aaae4e8130ae885888191b094fd310fe1160f680
jamesnelly <g00346996@gmit.ie>
2019-10-29 22:36:55 +0000

    Create ServerPassword.java

---
+++ ds/src/main/java/ie/gmit/sw/ds/ServerPassword.java
@@ -0,0 +1,36 @@
+package ie.gmit.sw.ds;
+
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class ServerPassword {
+
+	private io.grpc.Server grpcServer;
+	private static final Logger logger = Logger.getLogger(ServerPassword.class.getName());
+	private static final int PORT = 50551;
+
+	private void start() throws IOException {
+		grpcServer = ServerBuilder.forPort(PORT).addService(new PasswordServiceImpl()).build().start();
+		logger.info("Server started, listening on " + PORT);
+	}
+
+	private void stop() {
+		if (grpcServer != null) {
+			grpcServer.shutdown();
+		}
+	}
+
+	private void blockUntilShutdown() throws InterruptedException {
+		if (grpcServer != null) {
+			grpcServer.awaitTermination();
+		}
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		final ServerPassword Serverpassword = new ServerPassword();
+		Serverpassword.start();
+		Serverpassword.blockUntilShutdown();
+	}
+}
