ed8d194f1965b9b37a8f65fba994fc034fde902e
jamesnelly <g00346996@gmit.ie>
2019-10-30 09:51:08 +0000

    Server Updated

---
+++ ds/src/main/java/ie/gmit/sw/ds/Client.java
@@ -0,0 +1,102 @@
+package ie.gmit.sw.ds;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+
+public class Client {
+	
+	private static final Logger logger =
+    Logger.getLogger(Client.class.getName());
+    private final ManagedChannel channel;
+    private final PasswordServiceGrpc.PasswordServiceStub asyncPasswordService;
+    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncPasswordService;
+    private String UserPassword;
+    private ByteString HashedPassword;
+    private ByteString Salt;
+    private int UserID;
+    Scanner userInput = new Scanner(System.in);
+
+    public Client(String host, int port) {
+        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        syncPasswordService = PasswordServiceGrpc.newBlockingStub(channel);
+        asyncPasswordService = PasswordServiceGrpc.newStub(channel);
+    }
+    	public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void sendHashReq(){
+    	 System.out.println("Enter ID:");
+         UserID = userInput.nextInt();
+         System.out.println("Enter Password:");
+         UserPassword = userInput.next();
+
+        // Build HashRequest
+        System.out.println("Sending Hash Request");
+        HashRequest hashRequest = HashRequest.newBuilder().setUserID(UserID).setPassword(UserPassword).build();
+        // HashResponse
+        HashResponse hashResponse;
+
+        try {
+            hashResponse = syncPasswordService.hash(hashRequest);
+            HashedPassword = hashResponse.getHashedPassword();
+            Salt = hashResponse.getSalt();
+        }catch (StatusRuntimeException ex){
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
+    }
+
+    public void sendValidationRequest(){
+        StreamObserver<BoolValue> responseObserver = new StreamObserver<BoolValue>(){
+            @Override
+            public void onNext(BoolValue value) {
+                if(value.getValue()){
+                    System.out.println("Success");
+                }
+                else{
+                    System.out.println("Username or Password is incorrect");
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(" Error has occurred.. "+t.getLocalizedMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.exit(0);
+            }
+        };
+        try {
+            asyncPasswordService.validate(ValidateRequest.newBuilder().setPassword(UserPassword)
+                    .setHashedPassword(HashedPassword)
+                    .setSalt(Salt).build(), responseObserver);
+        } catch (StatusRuntimeException ex) {
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Client client = new Client("localhost", 50551);
+        try {
+            client.sendHashReq();
+            client.sendValidationRequest();
+            
+        } finally {
+            Thread.currentThread().join();
+        }
+    }
+}
--- ds/src/main/java/ie/gmit/sw/ds/PasswordServiceImpl.java
+++ ds/src/main/java/ie/gmit/sw/ds/PasswordServiceImpl.java
@@ -32,16 +32,14 @@ public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImpl
 	public void validate(ValidateRequest request, StreamObserver<BoolValue> responseObserver) {
 
 		try {
-			// isExpectedPassword(char[] password, byte[] salt, byte[] expectedHash)
+			// (isExpectedPassword(char[] password, byte[] salt, byte[] expectedHash)
 			char[] userPassword = request.getPassword().toCharArray();
 			byte[] hashedPassword = request.getHashedPassword().toByteArray();
 			byte[] salt = request.getSalt().toByteArray();
 
-			// Validate the hash matches and return true
 			if (Passwords.isExpectedPassword(userPassword, salt, hashedPassword)) {
 				responseObserver.onNext(BoolValue.newBuilder().setValue(true).build());
 			}
-			// Else return false
 			else {
 				responseObserver.onNext(BoolValue.newBuilder().setValue(false).build());
 			}
